bf4f072f56d5ab5deee3426744796a858fc8384c
yushuiquan <yushuiquan@ones.ai>
2022-01-07 18:09:55 +0800

    Update ExampleInstrumentedTest.java
    测试提交

--- app/src/androidTest/java/com/ysq/testdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ysq/testdemo/ExampleInstrumentedTest.java
@@ -16,6 +16,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+
+
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
